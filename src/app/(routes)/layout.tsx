6cfdfa4a3a02b82e5910f0b50c7b6399e8ab764f
rameshkasula <rameshlearningadvance@gmail.com>
2024-07-31 01:02:39 +0530

    path fixed

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -48,7 +48,10 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link href="/" className="flex items-center gap-2 font-semibold">
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-2 font-semibold"
+            >
               <Package2 className="h-6 w-6" />
               <span className="">Eraser.io</span>
             </Link>
