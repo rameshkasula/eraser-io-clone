3ae8fd76dd35a7251eadc4bd24fa969ec6f3d4aa
rameshkasula <rameshlearningadvance@gmail.com>
2024-08-02 21:57:02 +0530

    types fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,7 @@
 import nextAuthOptions from "@/utils/auth-helper";
 import NextAuth from "next-auth";
 
-import type { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = nextAuthOptions;
-
-const handler = NextAuth(authOptions);
+const handler = NextAuth(nextAuthOptions);
 
 export { handler as GET, handler as POST };
 
