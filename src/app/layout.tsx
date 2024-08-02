71ebb3dfc027da0bfccecd6876d853cfc376a4f6
rameshkasula <rameshlearningadvance@gmail.com>
2024-08-02 22:34:14 +0530

    fixes for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from "next"; // Remove if not used
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { AppSessionProvider } from "@/providers/session-provider";
-import { Session } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +15,10 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
   session,
-}: {
-  // @ts-ignore
+}: Readonly<{
   children: React.ReactNode;
-  // @ts-ignore
-  session: Session;
-}) {
+  session: any;
+}>) {
   return (
     <html lang="en">
       <body className={cn(inter.className, "min-h-screen")}>
